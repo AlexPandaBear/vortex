735add044f07791e19db349c657804810c031496
AlexPandaBear <alexandre.dutka@student.isae-supaero.fr>
2020-05-30 17:59:49 +0200

    update doc

--- src/DataManager.hxx
+++ src/DataManager.hxx
@@ -4,6 +4,9 @@
 #include <fstream>
 #include "Complex.hxx"
 
+/**
+ * A class managing simulation data storage
+ */
 class DataManager
 {
 private:
@@ -20,32 +23,208 @@ private:
 	std::unique_ptr<double[]> ptr_radiuses;
 
 public:
+	/**
+	 * The constructor of the class
+	 *
+	 * @param nb_steps The number of time steps of the simulation to save
+	 *
+	 * @param nb_vtx The number of vortices used in the simulation to save
+	 */
 	DataManager(size_t nb_steps, size_t nb_vtx);
+
+	/**
+	 * The destructor of the class
+	 */
 	~DataManager();
 
+	/**
+	 * Getter to the number of vortices data this instance can store
+	 *
+	 * @returns The number of vortices data this instance is currently built to store
+	 */
 	size_t getNbVtx() const;
+
+	/**
+	 * Getter to the number of steps this instance can store
+	 *
+	 * @returns The number of time steps this instance is currently built to store
+	 */
 	size_t getNbSteps() const;
 	
+	/**
+	 * Getter to the time corresponding to a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 */
 	double getTimeAt(size_t timeStep) const;
+	
+	/**
+	 * Getter to the fluid identification number of a specific vortex in the simulation stored in the instance
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	size_t getFluidId(size_t vortexID) const;
+
+	/**
+	 * Getter to the x-coordinate of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getXAt(size_t timeStep, size_t vortexID) const;
+	
+	/**
+	 * Getter to the y-coordinate of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getYAt(size_t timeStep, size_t vortexID) const;
+
+	/**
+	 * Getter to the projection of the velocity along the x-axis of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getUAt(size_t timeStep, size_t vortexID) const;
+
+	/**
+	 * Getter to the projection of the velocity along the y-axis of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getVAt(size_t timeStep, size_t vortexID) const;
+
+	/**
+	 * Getter to the velocity circulation of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getCirculationAt(size_t timeStep, size_t vortexID) const;
+
+	/**
+	 * Getter to the regularization radius of a specific vortex at a specific time step in the simulation stored in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 */
 	double getRegRadiusAt(size_t timeStep, size_t vortexID) const;
 
+	/**
+	 * Setter for the time corresponding to a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param time The time value to store
+	 */
 	void storeTimeAt(size_t timeStep, double time);
+
+	/**
+	 * Setter for the fluid identification number of a specific vortex to store in the instance
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param fluidId The value to store
+	 */
 	void storeFluidId(size_t vortexID, size_t fluidId);
+
+	/**
+	 * Setter for the x-coordinate of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param x The value to store
+	 */
 	void storeXAt(size_t timeStep, size_t vortexID, double x);
+	
+	/**
+	 * Setter for the y-coordinate of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param y The value to store
+	 */
 	void storeYAt(size_t timeStep, size_t vortexID, double y);
+
+	/**
+	 * Setter for the projection along the x-axis of the velocity of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param u The value to store
+	 */
 	void storeUAt(size_t timeStep, size_t vortexID, double u);
+
+	/**
+	 * Setter for the projection along the y-axis of the velocity of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param v The value to store
+	 */
 	void storeVAt(size_t timeStep, size_t vortexID, double v);
+	
+	/**
+	 * Setter for the velocity circulation of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param circulation The value to store
+	 */
 	void storeCirculationAt(size_t timeStep, size_t vortexID, double circulation);
+
+	/**
+	 * Setter for the regularization radius of a specific vortex at a specific time step to store in the instance
+	 *
+	 * @param timeStep The number of the time step
+	 *
+	 * @param vortexID The number of the vortex
+	 *
+	 * @param radius The value to store
+	 */
 	void storeRegRadiusAt(size_t timeStep, size_t vortexID, double radius);
 
+	/**
+	 * Method writing the current state of the instance in a file in order to be able to reconstruct the same instance later with the loadFile method
+	 *
+	 * @param fileNameWithPath The name of the file with its path
+	 */
 	void saveState(std::string fileNameWithPath) const;
+
+	/**
+	 * Method loading the data previously stored in a file with the method saveState in order to restore a previous state or another instance
+	 *
+	 * @param fileNameWithPath The name of the file with its path
+	 */
 	void loadFile(std::string fileNameWithPath);
 
+	/**
+	 * Method resizing the different arrays in order to store a simulation with a new number of vortices or a new number of time steps
+	 *
+	 * @param nb_timeSamples The new number of time samples
+	 *
+	 * @param nb_vtx The new number of vortices
+	 *
+	 * @warning nb_timeSamples = nb_steps + 1
+	 */
 	void reset(size_t nb_timeSamples, size_t nb_vtx);
 };
--- src/SimKernel.hxx
+++ src/SimKernel.hxx
@@ -8,6 +8,9 @@
 #include "DataAnalyst.hxx"
 #include "Vortex.hxx"
 
+/**
+ * A class implementing the kernel of the Vortex Method
+ */
 class SimKernel
 {
 private:
@@ -46,15 +49,77 @@ private:
 	void printSimProgression(size_t step) const;
 
 public:
+	/**
+	 * The constructor of the class
+	 *
+	 * @param x_periodic A boolean telling if the simulation is x-periodic
+	 *
+	 * @param y_periodic A boolean telling if the simulation is y-periodic \todo
+	 *
+	 * @param x_period The value of the eventual spatial period along the x-axis
+	 *
+	 * @param y_period The value of the eventual spatial period along the y-axis
+	 *
+	 * @param method The name of the time stepping integration method (Possible values : 'euler', 'rk4', 'eulerA', 'eulerB', 'sv', 'svi')
+	 */
 	SimKernel(bool x_periodic, bool y_periodic, double x_period, double y_period, std::string method);
+	
+	/**
+	 * The destructor of the class	 
+	 */
 	~SimKernel();
 
+	/**
+	 * Setter for the x-periodicity of the instance
+	 * 
+	 * @param periodic A boolean telling if computation should be periodic along the x-axis
+	 *
+	 * @param period The value of the eventual spatial period
+	 */
 	void setXPeriodicityTo(bool periodic, double period);
+
+	/**
+	 * Setter for the y-periodicity of the instance
+	 * 
+	 * @param periodic A boolean telling if computation should be periodic along the y-axis
+	 *
+	 * @param period The value of the eventual spatial period
+	 */
 	void setYPeriodicityTo(bool periodic, double period);
+
+	/**
+	 * Setter for the time stepping integration method to use
+	 *
+	 * @param method The name of the method (Possible values : 'euler', 'rk4', 'eulerA', 'eulerB', 'sv', 'svi')
+	 */
 	void setMethodTo(std::string method);
 	
+	/**
+	 * Method adding a vortex to the list of vortex that will be used for computation
+	 *
+	 * @param v The vortex to add to the list
+	 */
 	void addVortex(Vortex &v);
+
+	/**
+	 * Method building the list of the time-values used for computation
+	 *
+	 * @param t0 The time at which the simulation will start
+	 *
+	 * @param tEnd The time at which the simulation will end (must be greater than t0)
+	 *
+	 * @param steps The number of time steps to simulate from t0 to tEnd (must be greater than 1)
+	 */
 	void buildTimeSample(double t0, double tEnd, size_t steps);
 
+	/**
+	 * Method simulating the flow with all the parameters previously defined
+	 *
+	 * @param dm The DataManager object in which the simulation will be stored
+	 *
+	 * @param nb_threads The number of threads to use to speed-up computation (must be greater than 1)
+	 *
+	 * @warning All the vortices must be defined and added to the simulation (with the addVortex method), and the time sample must be defined (with the buildTimeSample method) before calling this method
+	 */
 	void sim(DataManager &dm, size_t nb_threads);
 };
